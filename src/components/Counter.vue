3c3cbafce7bf63b17e7090ee05c28a03334a04fe
fcamblor2 <fcamblor@gmail.com>
2024-11-07 13:45:47 +0100

    step1: introduced Counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,13 @@
+<template>
+  <button type="button" @click="count++">count is {{ count }}</button>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const count = ref(0)
+</script>
+
+<style scoped>
+
+</style>
