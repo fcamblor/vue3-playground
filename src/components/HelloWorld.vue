87ea4ef8576a971803209d9de58a1df23fe91ea0
fcamblor2 <fcamblor@gmail.com>
2024-11-07 01:01:12 +0100

    step1: added an 'external' (ref or non ref) variable that might be manipulated outside vue scope
    including arrys, objects, nested objects..

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,25 @@ import { ref } from 'vue'
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
+const count = ref(0);
+(window as any).count = count;
+
+const test = { value: 10 };
+// try to edit window.test.value in console and see how this will not be reflected into the DOM
+// ... until a new re-render is triggered (like, clicking on count button)
+(window as any).test = test;
+
+const arr = ref<string[]>([]);
+(window as any).arr = arr;
+
+const obj = ref<Record<string,string>>({});
+(window as any).obj = obj;
+
+const nestedObj = ref<{nested: Record<string,string>}>({nested:{}});
+(window as any).nestedObj = nestedObj;
+
+
+console.log(`in HelloWorld <script>`)
 </script>
 
 <template>
@@ -11,6 +29,10 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
+    {{test.value}}
+    {{arr.length}}
+    {{Object.keys(obj).length}}
+    {{Object.keys(nestedObj.nested).length}}
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
